e39373a4bc436102e9d16ba86e68740ade9ae302
solunasalles <m.a.nagorny@gmail.com>
2020-06-27 19:06:03 +0500

    homework03

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField('Название', max_length=128, unique=True)
+    description = models.TextField('Описание', max_length=255, blank=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Products(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT, verbose_name='Категория')
+    name = models.CharField('Название', max_length=128)
+    short_description = models.CharField('Краткое описание', max_length=128, blank=True)
+    description = models.TextField('Описание', blank=True)
+    quantity = models.PositiveIntegerField('Количество', default=0)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=0, default=0)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField('Дата обновления', auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
